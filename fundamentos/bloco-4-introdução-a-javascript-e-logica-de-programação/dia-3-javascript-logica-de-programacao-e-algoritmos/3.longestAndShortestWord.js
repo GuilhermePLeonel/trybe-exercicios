08f9cd09355fdba1af858879f7420ac9151d54ff
GuilhermePLeonel <guilhermepleonel1997@gmail.com>
2022-03-09 16:58:48 -0300

    exercicio 3 p2

--- fundamentos/bloco-4-introdução-a-javascript-e-logica-de-programação/dia-3-javascript-logica-de-programacao-e-algoritmos/3.longestAndShortestWord.js
+++ fundamentos/bloco-4-introdução-a-javascript-e-logica-de-programação/dia-3-javascript-logica-de-programacao-e-algoritmos/3.longestAndShortestWord.js
@@ -1,3 +1,5 @@
+//  LongestWord
+
 let skills = ["java", "paralelepipedo", "javascript", "python", "html", "css"];
 let longest = skills[0];
 
@@ -10,4 +12,22 @@ for (let index = 0; index < skills.length; index += 1) {
   }
 }
 
-console.log(longest);
+console.log("The longest word is:" + " " + longest);
+
+
+
+// ShortestWord
+
+
+let shortest = skills[0];
+
+for (let index = 0; index < skills.length; index += 1) {
+  if (skills[index].length < shortest.length) {
+    shortest = skills[index];
+  } 
+  else {
+
+  }
+}
+
+console.log("The shortest word is:" + " " + shortest);
