c3c4083ae86dcad98eba5d84b426edcd558e31f3
GuilhermePLeonel <guilhermepleonel1997@gmail.com>
2022-03-09 19:15:31 -0300

    bonus exercises 1

--- fundamentos/bloco-4-introdução-a-javascript-e-logica-de-programação/dia-3-javascript-logica-de-programacao-e-algoritmos/4.primeNumbers.js
+++ fundamentos/bloco-4-introdução-a-javascript-e-logica-de-programação/dia-3-javascript-logica-de-programacao-e-algoritmos/4.primeNumbers.js
@@ -3,44 +3,16 @@ let aux = [0];
 let divisions = 0;
 
 for (let index = 2; index <= number; index += 1) {
-    divisions = 0;
-    for (let index2 = 1; index2 <= index; index2 += 1) {
-       if (index%index2===0) {
-        divisions += 1
-        }
-        
+  divisions = 0;
+  for (let index2 = 1; index2 <= index; index2 += 1) {
+    if (index % index2 === 0) {
+      divisions += 1;
     }
-    
+  }
 
-    if (divisions <= 2){
-        aux [0] = index;
-    }
-    
-    
-
+  if (divisions <= 2) {
+    aux[0] = index;
+  }
 }
 
 console.log(aux);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
---
+++ fundamentos/bloco-4-introdução-a-javascript-e-logica-de-programação/dia-3-javascript-logica-de-programacao-e-algoritmos/bonus/1.quadrado.js
@@ -0,0 +1,13 @@
+let n = 5;
+let aux = "";
+
+for (let index = 0; index < n; index += 1){
+    aux = "";
+    for (let index2 = 0; index2 < n; index2 += 1){
+      aux += "*";  
+    }
+    console.log(aux);
+     
+}
+
+
