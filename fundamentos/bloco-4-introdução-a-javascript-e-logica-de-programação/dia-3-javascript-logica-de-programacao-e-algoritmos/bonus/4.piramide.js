0c33fe02f99fdade36f7ecf434b0be46d91185a0
GuilhermePLeonel <guilhermepleonel1997@gmail.com>
2022-03-09 21:51:17 -0300

    bonus exercises 4

--- fundamentos/bloco-4-introdução-a-javascript-e-logica-de-programação/dia-3-javascript-logica-de-programacao-e-algoritmos/bonus/4.piramide.js
+++ fundamentos/bloco-4-introdução-a-javascript-e-logica-de-programação/dia-3-javascript-logica-de-programacao-e-algoritmos/bonus/4.piramide.js
@@ -1,10 +1,10 @@
-let n = 20;
+let n = 5;
 let aux = "";
 
-for (let index = 0; index < n; index += 1) {
+for (let index = 0; index < n; index += 2) {
   aux = "";
   for (let index2 = 0; index2 <= n; index2 += 1) {
-    if(index + index2 > n-1){
+    if(){
         aux += "*";
     }
     else{
@@ -13,3 +13,4 @@ for (let index = 0; index < n; index += 1) {
   }
   console.log(aux);
 }
+                              
