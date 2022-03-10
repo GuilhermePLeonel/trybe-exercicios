5f3392b210ec7b5a807b32dc52c6e64fc2ebb392
GuilhermePLeonel <guilhermepleonel1997@gmail.com>
2022-03-10 16:57:22 -0300

    exercicise 2

---
+++ fundamentos/bloco-4-introdução-a-javascript-e-logica-de-programação/dia-4-javascript-e-objetos-e-funcoes/funcoes/2.higherValue.js
@@ -0,0 +1,14 @@
+function higherValue(array) {
+  let aux = [0];  
+  for (let index = 0; index < array.length; index += 1) {
+    if(array[index] > aux[0]){
+        aux[0] = array[index];
+    }
+  }
+  return array.indexOf(aux[0]);
+}
+
+
+console.log(higherValue([2, 3, 6, 7, 10, 18]));
+
+
